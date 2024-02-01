4f090bc1dc44856c4d9cabb31f9c6de18c0ed1ca
Lum <supakorn.s@protonmail.com>
2024-02-01 21:57:12 +0700

    Reformat smartPointers

--- exercises/smartPointers/smartPointers.cpp
+++ exercises/smartPointers/smartPointers.cpp
@@ -21,7 +21,8 @@
  * In the *advanced course*:
  * - Work on problem1() to problem4()
  *
- * In main() at the bottom, comment in the different parts as you progress through the exercise.
+ * In main() at the bottom, comment in the different parts as you progress through the
+ * exercise.
  *
  * Remember that:
  * - The unique ownership of data is expressed using unique_ptr.
@@ -49,10 +50,9 @@ double sumEntries(std::span<double const> range) {
     return std::reduce(range.begin(), range.end());
 }
 
-// Often, data are owned by one entity, and merely used by others. In this case, we hand the data to
-// sumEntries() for reading, so the ownership stays with this function. Unfortunately, something goes
-// wrong and we didn't use smart pointers.
-// Understand and fix the memory leak.
+// Often, data are owned by one entity, and merely used by others. In this case, we hand the
+// data to sumEntries() for reading, so the ownership stays with this function. Unfortunately,
+// something goes wrong and we didn't use smart pointers. Understand and fix the memory leak.
 void doStuffWithData() {
     auto data = new std::array<double, 10000>{};
 
@@ -76,18 +76,22 @@ void problem1() {
 /* --------------------------------------------------------------------------------------------
  * 2: Storing unique_ptr in collections.
  *
- * Often, one has to store pointers to objects in collections. Fix the memory leaks using unique_ptr.
+ * Often, one has to store pointers to objects in collections. Fix the memory leaks using
+ * unique_ptr.
  *
  * Notes:
  * - Factory functions should return objects either directly or using smart pointers.
- *   This is good practice, because it clearly shows who owns an object. Fix the return type of the factory function.
+ *   This is good practice, because it clearly shows who owns an object. Fix the return type
+ *   of the factory function.
  * - The vector should own the objects, so try to store them using smart pointers.
- * - Since the change function doesn't accept smart pointers, find a solution to pass the objects.
+ * - Since the change function doesn't accept smart pointers, find a solution to pass the
+ *   objects.
  *   Note that this works without shared_ptr!
  * --------------------------------------------------------------------------------------------
  */
 
-// This is a large object. We maybe shouldn't copy it, so using a pointer is advisable to pass it around.
+// This is a large object. We maybe shouldn't copy it, so using a pointer is advisable to
+// pass it around.
 struct LargeObject {
     std::array<double, 100000> fData;
 };
@@ -128,15 +132,17 @@ void problem2() {
  * Most of the time, ownership can be solved by having one owner (with unique_ptr) and one or
  * more observers (raw pointers or references). Sometimes, we need to truly share data, though.
  *
- * Here is an example of a completely messed up ownership model. It leaks about 1/10 of the times
- * it is invoked.
+ * Here is an example of a completely messed up ownership model. It leaks about 1/10 of the
+ * times it is invoked.
  * - Verify this by running it in a loop using a command like:
- * while true; do valgrind --leak-check=full --track-origins=yes ./smartPointers 2>&1 | grep -B 5 -A 5 problem3 && exit 1; done
+ * while true; do valgrind --leak-check=full --track-origins=yes ./smartPointers 2>&1 |
+ * grep -B 5 -A 5 problem3 && exit 1; done
  * - Fix the ownership model using shared_ptr!
  *   - Convert the vectors to holding shared_ptr.
  *   - Fix the arguments of the functions.
  *   - Speed optimisation:
- *     Make sure that you don't create & destroy a shared_ptr in the for loop in problem3() and when calling processElement().
+ *     Make sure that you don't create & destroy a shared_ptr in the for loop in problem3()
+ *     and when calling processElement().
  * --------------------------------------------------------------------------------------------
  */
 
@@ -209,8 +215,8 @@ void problem3() {
  * Class members that are pointers can quickly become a problem.
  * Firstly, if only raw pointers are used, the intended ownership is unclear.
  * Secondly, it's easy to overlook that a member has to be deleted.
- * Thirdly, pointer members usually require you to implement copy or move constructors and assignment
- * operators (--> rule of 3, rule of 5).
+ * Thirdly, pointer members usually require you to implement copy or move constructors and
+ * assignment operators (--> rule of 3, rule of 5).
  * Since C++-11, one can solve a few of those problems using smart pointers.
  *
  * 4.1:
@@ -220,30 +226,34 @@ void problem3() {
  *
  * Tasks:
  * - Comment in problem4_1() in main().
- * - It likely crashes. Verify this. You can also try running valgrind ./smartPointers, it should give you some hints as to
- *   what's happening.
- * - Fix the Owner class by using a shared_ptr for its _largeObj, which we can copy as much as we want.
+ * - It likely crashes. Verify this. You can also try running valgrind ./smartPointers, it
+ *   should give you some hints as to what's happening.
+ * - Fix the Owner class by using a shared_ptr for its _largeObj, which we can copy as much
+ *   as we want.
  * - Run the fixed program.
  * - Note: Once shared_ptr is in use, you can also use the default destructor.
  *
  * 4.2: **BONUS**
  * Let's use a weak_ptr now to observe a shared_ptr.
- * These are used to observe a shared_ptr, but unlike the shared_ptr, they don't prevent the deletion
- * of the underlying object if all shared_ptr go out of scope.
- * To *use* the observed data, one has to create a shared_ptr from the weak_ptr, so that it is guaranteed that
- * the underlying object is alive.
+ * These are used to observe a shared_ptr, but unlike the shared_ptr, they don't prevent the
+ * deletion of the underlying object if all shared_ptr go out of scope.
+ * To *use* the observed data, one has to create a shared_ptr from the weak_ptr, so that it
+ * is guaranteed that the underlying object is alive.
  *
- * In our case, the observer class wants to observe the data of the owner, but it doesn't need to own it.
+ * In our case, the observer class wants to observe the data of the owner, but it doesn't
+ * need to own it.
  * To do this, we use a weak pointer.
  *
  * Tasks:
  * - Comment in problem4_2() in main().
- * - Investigate the crash. Use a debugger, run in valgrind, compile with -fsanitize=address ...
- * - Rewrite the interface of Owner::getData() such that the observer can see the shared_ptr to the large object.
+ * - Investigate the crash. Use a debugger, run in valgrind, compile with -fsanitize=address
+ * - Rewrite the interface of Owner::getData() such that the observer can see the shared_ptr
+ *   to the large object.
  * - Set up the Observer such that it stores a weak pointer that observes the large object.
- * - In Observer::processData(), access the weak pointer, and use the data *only* if the memory is still alive.
- *   Note: What you need is weak_ptr::lock(). Check out the documentation and the example at the bottom:
- *   https://en.cppreference.com/w/cpp/memory/weak_ptr/lock
+ * - In Observer::processData(), access the weak pointer, and use the data *only* if the
+ *   memory is still alive.
+ *   Note: What you need is weak_ptr::lock(). Check out the documentation and the example at
+ *   the bottom: https://en.cppreference.com/w/cpp/memory/weak_ptr/lock
  * --------------------------------------------------------------------------------------------
  */
 
